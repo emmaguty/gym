4314075a7b5db2184cbc68c91b95bc6bd8fe1da8
emmanuelguty <manuel15901@gmail.com>
2022-12-18 17:03:53 -0600

    Navbar component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
         <>
             <Navbar />
         </>
-    )
-}
+    );
+};
 
 export default App;
--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,12 +1,37 @@
 import {NavLink} from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
     return(
         <>
             <nav className="navbar navbar-expand bg-dark">
                 <NavLink to="/" className="navbar-brand">
-                    GART GYM
+                    <span>GART</span> GYM
                 </NavLink>
+                <div>
+                    <ul className="navbar-nav">
+                        <li className='nav-item'>
+                            <NavLink to='/' className="nav-link">
+                                Inicio
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink to='/' className="nav-link">
+                                Acerca
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink to='/' className="nav-link">
+                                Entrenamientos
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink to='/' className="nav-link">
+                                Servicios
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </>
     )
